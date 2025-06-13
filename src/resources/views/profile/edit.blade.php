0c4e221ddc6887aaffdd03e5919d3d98e67ac79b
ayana-suenobu <ayana-suenobu@comthink.co.jp>
2025-06-13 14:53:10 +0900

    マイページ編集画面の配置変更など

--- src/resources/views/profile/edit.blade.php
+++ src/resources/views/profile/edit.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('マイページ編集') }}
         </h2>
     </x-slot>
 
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-gray-50 shadow sm:rounded-lg">
+                @if ($user->profile_image)
+                <div class="flex justify-start mb-4">
+                    <img src="{{ asset('storage/' . $user->profile_image) }}" alt="Profile Image" class="w-32 h-32 rounded-full object-cover">
+                </div>
+                @endif
                 
 
            <div class="max-w-xl">
@@ -16,13 +21,13 @@
                 </div>
            </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-gray-50 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-gray-50 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>

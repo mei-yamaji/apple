7f42b91a15b0b18f55280ad7b7da6f2c2409462a
ayana-suenobu <ayana-suenobu@comthink.co.jp>
2025-06-16 09:41:50 +0900

    ログイン画面のUI変更

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,10 +1,4 @@
 <x-guest-layout>
-    <div class="flex items-center justify-center bg-yellow-50 overflow-hidden">
-        <!-- 背景画像（切れないように表示） -->
-        <img src="https://4.bp.blogspot.com/-uY6ko43-ABE/VD3RiIglszI/AAAAAAAAoEA/kI39usefO44/s800/fruit_ringo.png" 
-            alt="中央画像" 
-            class="absolute top-0 left-0 w-full h-full object-contain opacity-70">
-
         <!-- ログインフォーム（透明背景） -->
         <div class="z-10 p-8 rounded-lg  max-w-md w-full">
             <!-- Session Status -->
@@ -15,14 +9,14 @@
 
                 <!-- Email Address -->
                 <div>
-                    <x-input-label for="email" :value="__('Email')" />
-                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-label for="email" :value="__('Email')" class="text-yellow-50"/>
+                    <x-text-input id="email" class="block mt-1 w-full " type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
 
                 <!-- Password -->
                 <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
+                    <x-input-label for="password" :value="__('Password')" class="text-yellow-50"/>
                     <x-text-input id="password" class="block mt-1 w-full"
                         type="password"
                         name="password"
@@ -33,25 +27,25 @@
                 <!-- Remember Me -->
                 <div class="block mt-4">
                     <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-green-500 shadow-sm focus:ring-indigo-400" name="remember">
+                        <span class="ms-2 text-sm text-yellow-50">{{ __('Remember me') }}</span>
                     </label>
                 </div>
                 <div class="block mt-4">
                     <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                        <span class="ms-2 text-sm text-gray-600">{{ __('Are you RUNTEQ?') }}</span>
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-green-500 shadow-sm focus:ring-indigo-400" name="remember">
+                        <span class="ms-2 text-sm text-yellow-50">{{ __('Are you RUNTEQ?') }}</span>
                     </label>
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
                     @if (Route::has('register'))
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-3" href="{{ route('register') }}">
+                        <a class="underline text-sm text-yellow-50 hover:text-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-3" href="{{ route('register') }}">
                             {{ __('Please Register here') }}
                         </a>
                     @endif
                     @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        <a class="underline text-sm text-yellow-50 hover:text-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                             {{ __('Forgot your password?') }}
                         </a>
                     @endif

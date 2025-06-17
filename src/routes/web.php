20973ee14bb292e7ec8045ab7198f63d48b897ff
yumeka-mori@comthink.co.jp <yumeka-mori@comthink.co.jp>
2025-06-17 09:28:03 +0900

    書き直し

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BoardController;
 use Illuminate\Support\Facades\Route;
@@ -7,44 +7,44 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\CommentController;
 use App\Models\Board;
-
+ 
 Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
+ 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+ 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+ 
     Route::resource('boards', BoardController::class);
     Route::get('/boards/{type}', [BoardController::class, 'getBoards']);
     Route::get('/boards/{board}', [BoardController::class, 'show'])->name('boards.show');
     Route::post('/likes', [LikeController::class, 'store'])->name('likes.store');
     Route::delete('/likes/{board}', [LikeController::class, 'destroy'])->name('likes.destroy');
-
+ 
     Route::post('boards/{board}/comments', [CommentController::class, 'store'])->name('comments.store');
     Route::get('/boards/{board}/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
     Route::put('/boards/{board}/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
     Route::delete('/boards/{board}/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
-
+ 
     Route::get('/mypage', [UserController::class, 'mypage'])->name('mypage');
     Route::get('/users/{user}', [UserController::class, 'show'])->name('user.show');
-
-
+ 
+ 
     Route::get('/mypage', function () {
     $boards = Board::where('user_id', auth()->id())->latest()->get();
     return view('mypage', compact('boards'));
 })->name('mypage');
-
-
+ 
+ 
 });
-
-
-
-
+ 
+ 
+ 
+ 
 require __DIR__.'/auth.php';

53dfc75ff3fb88fe8d56bd8b9e9547de431fd613
yumeka-mori@comthink.co.jp <yumeka-mori@comthink.co.jp>
2025-06-18 16:53:50 +0900

    RUNTEQの認証

--- src/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,14 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'runteq_student' => ['nullable', 'boolean'], 
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'is_runteq_student' => $request->boolean('runteq_student'),
         ]);
 
         event(new Registered($user));

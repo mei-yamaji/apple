cee369dd3d690e2e0a9e0433779f64ed259b3592
yumeka-mori@comthink.co.jp <yumeka-mori@comthink.co.jp>
2025-06-19 16:49:47 +0900

    プロフィール画面でも受講生にマーク

--- src/resources/views/user/show.blade.php
+++ src/resources/views/user/show.blade.php
@@ -9,17 +9,22 @@
   <div class="max-w-xl mx-auto mt-6 p-4 bg-white shadow rounded">
 
     @if ($user->profile_image)
-<img src="{{ asset('storage/' . $user->profile_image) }}" alt="Profile Image" class="w-32 h-32 rounded-full object-cover mx-auto mb-4">
+    <img src="{{ asset('storage/' . $user->profile_image) }}" alt="Profile Image" class="w-32 h-32 rounded-full object-cover mx-auto mb-4">
 
     @else
-<div class="w-32 h-32 rounded-full bg-gray-300 flex items-center justify-center mx-auto mb-4">
-<span class="text-gray-500">No Image</span>
-</div>
-
+    <div class="w-32 h-32 rounded-full bg-gray-300 flex items-center justify-center mx-auto mb-4">
+      <span class="text-gray-500">No Image</span>
+    </div>
     @endif
  
-    <p class="text-lg font-semibold text-center">{{ $user->name }}</p>
-<p class="text-gray-600 text-center mt-2">{{ $user->bio }}</p>
+    <p class="text-lg font-semibold text-center">
+        {{ $user->name }}
+        @if ($user->is_runteq_student)
+                <span>🍎</span>
+        @endif
+    </p>
+
+    <p class="text-gray-600 text-center mt-2">{{ $user->bio }}</p>
 </div>
 </x-app-layout>
  

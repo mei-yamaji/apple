992e0cbaa2b2b1effe44614f508110abb7a2ad75
mei-yamaji <mei-yamaji@comthink.co.jp>
2025-06-26 10:03:33 +0900

    UI変更

--- src/resources/views/boards/preview-edit.blade.php
+++ src/resources/views/boards/preview-edit.blade.php
@@ -37,10 +37,10 @@
         <div class="flex justify-between items-center">
   {{-- 編集に戻るリンク --}}
   <a href="{{ route('boards.edit', $board->id) }}"
-     class="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-orange-500 bg-white border border-orange-400 rounded-full shadow hover:bg-orange-100 hover:scale-105 transition-all duration-200">
-    <i class="ri-arrow-go-back-line mr-1"> 戻る
-    </i>
-  </a>
+   class="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-orange-500 bg-white border border-orange-400 rounded-full shadow hover:bg-orange-100 hover:scale-105 transition-all duration-200">
+    <i class="ri-arrow-go-back-line mr-1"></i> 戻る
+</a>
+
 
   {{-- 更新ボタン（フォーム内） --}}
   <x-primary-button type="submit">
